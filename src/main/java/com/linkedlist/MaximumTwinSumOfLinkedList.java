f55e5992c4681f9e2074567dce1c87e7e753dd1c
kalbandeSwapnil <swapnilkalbande10@gmail.com>
2025-01-02 16:08:33 +0530

    Leetcode 75:2130. Maximum Twin Sum of a Linked List

---
+++ src/main/java/com/linkedlist/MaximumTwinSumOfLinkedList.java
@@ -0,0 +1,90 @@
+package com.linkedlist;
+
+/*
+2130. Maximum Twin Sum of a Linked List
+
+In a linked list of size n, where n is even, the ith node (0-indexed) of the linked list is known as the twin of the (n-1-i)th node, if 0 <= i <= (n / 2) - 1.
+
+For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the twin of node 2. These are the only nodes with twins for n = 4.
+The twin sum is defined as the sum of a node and its twin.
+
+Given the head of a linked list with even length, return the maximum twin sum of the linked list.
+
+
+
+Example 1:
+
+
+Input: head = [5,4,2,1]
+Output: 6
+Explanation:
+Nodes 0 and 1 are the twins of nodes 3 and 2, respectively. All have twin sum = 6.
+There are no other nodes with twins in the linked list.
+Thus, the maximum twin sum of the linked list is 6.
+Example 2:
+
+
+Input: head = [4,2,2,3]
+Output:
+ */
+public class MaximumTwinSumOfLinkedList {
+
+    public int pairSum(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode secondHalf = reverseList(slow);
+
+        ListNode firstHalf = head;
+        int maxTwinSum = 0;
+        while (secondHalf != null) {
+            maxTwinSum = Math.max(maxTwinSum, firstHalf.val + secondHalf.val);
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return maxTwinSum;
+    }
+
+
+    public ListNode reverseList(ListNode head) {
+        ListNode previous = null;
+        ListNode current = head;
+        ListNode next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        head = previous;
+        return head;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+
+        MaximumTwinSumOfLinkedList maximumTwinSumOfLinkedList = new MaximumTwinSumOfLinkedList();
+        // Example 2: [5,4,2,1]
+        ListNode head2 = new ListNode(5, new ListNode(4, new ListNode(2, new ListNode(1, null))));
+        System.out.println("Original List:");
+        printList(head2);
+        int result2 = maximumTwinSumOfLinkedList.pairSum(head2);
+        System.out.println("max sum of List:" + result2);
+
+
+    }
+}
