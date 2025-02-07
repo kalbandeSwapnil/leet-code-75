29869b42fae54eac0fb0de0a9af25577c798c37b
kalbandeSwapnil <swapnilkalbande10@gmail.com>
2025-02-07 21:03:26 +0530

    Leetcode 75: fast pointer and slow pointer problem

---
+++ src/main/java/com/pattern/fastandslowpointer/LinkedListCycle.java
@@ -0,0 +1,38 @@
+package com.pattern.fastandslowpointer;
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class LinkedListCycle {
+
+    public boolean hasCycle(ListNode head) {
+        ListNode fast, slow;
+
+        fast = slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
